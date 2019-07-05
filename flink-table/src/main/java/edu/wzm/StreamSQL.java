ee797dd971a79fc32de6376547682d2642c5aa37
wangzhimingjimmy <wangzhimingjimmy@didichuxing.com>
2019-07-05 20:29:55 +0800

    stream sql with java

---
+++ flink-table/src/main/java/edu/wzm/StreamSQL.java
@@ -0,0 +1,72 @@
+package edu.wzm;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+
+import java.util.Arrays;
+
+public class StreamSQL {
+    public static void main(String[] args) throws Exception {
+
+        // set up execution environment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tEnv = TableEnvironment.getTableEnvironment(env);
+
+        DataStream<Order> orderA = env.fromCollection(Arrays.asList(
+                new Order(1L, "beer", 3),
+                new Order(1L, "diaper", 4),
+                new Order(3L, "rubber", 2)));
+
+        DataStream<Order> orderB = env.fromCollection(Arrays.asList(
+                new Order(2L, "pen", 3),
+                new Order(2L, "rubber", 3),
+                new Order(4L, "beer", 1)));
+
+        // convert DataStream to Table
+        Table tableA = tEnv.fromDataStream(orderA, "user, product, amount");
+        // register DataStream as Table
+        tEnv.registerDataStream("OrderB", orderB, "user, product, amount");
+
+        // union the two tables
+        Table result = tEnv.sqlQuery("SELECT * FROM " + tableA + " WHERE amount > 2 UNION ALL " +
+                "SELECT * FROM OrderB WHERE amount < 2");
+
+        tEnv.toAppendStream(result, Order.class).print();
+
+        env.execute();
+    }
+
+    // *************************************************************************
+    //     USER DATA TYPES
+    // *************************************************************************
+
+    /**
+     * Simple POJO.
+     */
+    public static class Order {
+        public Long user;
+        public String product;
+        public int amount;
+
+        public Order() {
+        }
+
+        public Order(Long user, String product, int amount) {
+            this.user = user;
+            this.product = product;
+            this.amount = amount;
+        }
+
+        @Override
+        public String toString() {
+            return "Order{" +
+                    "user=" + user +
+                    ", product='" + product + '\'' +
+                    ", amount=" + amount +
+                    '}';
+        }
+    }
+}
